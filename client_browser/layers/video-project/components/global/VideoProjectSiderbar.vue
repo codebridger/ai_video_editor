b541cc97a3b95cc4b28a6e51ad555b1b1f194b6f
Navid Shad <navidshad72@gmail.com>
2024-06-09 21:05:33 +0300

    refactor: Remove unused VideoProjectSiderbar component and update sidebar navigation

--- client_browser/layers/video-project/components/global/VideoProjectSiderbar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <span> Sidebar </span>
-</template>
--- client_browser/layers/video-project/composables/projectLayoutUtils.ts
+++ client_browser/layers/video-project/composables/projectLayoutUtils.ts
@@ -3,6 +3,7 @@ import type { SidebarLayoutConfig } from "../types/collapseConfig.type";
 export function useSidebarSetup() {
   const appConfig = useAppConfig();
   const { t } = useI18n();
+  const router = useRouter();
 
   return {
     setupSidebarLayout(id: string) {
@@ -30,21 +31,25 @@ export function useSidebarSetup() {
             {
               title: "Timeline",
               activePath: `/project-${id}/timeline`,
-              to: `/project-${id}/timeline`,
               icon: { name: "i-ph-film-strip-duotone", class: "w-5 h-5" },
               subsidebar: {
-                component: "VideoProjectSiderbar",
+                component: "VideoProjectSidebar",
+              },
+              click: () => {
+                router.push(`/project-${id}/timeline`);
               },
             },
-            // {
-            //   title: "Shorts",
-            //   activePath: `/project-${id}/shorts`,
-            //   to: `/project-${id}/shorts`,
-            //   icon: { name: "i-ph-film-strip-duotone", class: "w-5 h-5" },
-            //   subsidebar: {
-            //     component: null,
-            //   },
-            // },
+            {
+              title: "Shorts And Story",
+              activePath: `/project-${id}/shorts`,
+              icon: { name: "i-tdesign-mobile-shortcut", class: "w-5 h-5" },
+              subsidebar: {
+                component: "VideoProjectSidebar",
+              },
+              click: () => {
+                router.push(`/project-${id}/shorts`);
+              },
+            },
           ],
         },
       };
