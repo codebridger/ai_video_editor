6224819a50d376c82c4c61f017d0cc437f484a1a
Navid Shad <navidshad72@gmail.com>
2025-03-04 13:27:36 +0300

    #86erm2kr7 - refactor: Update database types to use constants from server configuration

--- client_browser_v2/types/database.type.ts
+++ client_browser_v2/types/database.type.ts
@@ -1,17 +1,22 @@
 import { type FileDocument } from '@modular-rest/client/dist/types/types';
+import { USER_CONTENT, VIDEO_PROJECT } from '../../server/src/config';
 
 export const DATABASE = {
-    USER_CONTENT: 'user_content',
+  USER_CONTENT: USER_CONTENT.DATABASE,
+  VIDEO_PROJECT: VIDEO_PROJECT.DATABASE,
 };
 
 export const COLLECTIONS = {
-    PROFILE: 'profile',
+  PROFILE: USER_CONTENT.PROFILE_COLLECTION,
+  PROJECT_COLLECTION: VIDEO_PROJECT.PROJECT_COLLECTION,
+  VIDEO_MEDIA: VIDEO_PROJECT.VIDEO_MEDIA,
+  VIDEO_REVISION: VIDEO_PROJECT.VIDEO_REVISION,
 };
 
 export interface ProfileType {
-    _id: string;
-    refId: string;
-    name: string;
-    gPicture: string;
-    images: FileDocument[];
+  _id: string;
+  refId: string;
+  name: string;
+  gPicture: string;
+  images: FileDocument[];
 }
