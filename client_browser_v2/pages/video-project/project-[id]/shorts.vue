bfea89000e6c447de059ea3e26cab5f8a8aaac70
SomiVista <somayerohani06@gmail.com>
2025-03-12 21:13:20 +0400

    #86erm2kra- Add layout video project

---
+++ client_browser_v2/pages/video-project/project-[id]/shorts.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ t('video-project.shorts') }}</h1>
+</template>
+
+<script setup lang="ts">
+    const { t } = useI18n();
+    definePageMeta({
+        layout: 'project',
+        title: () => t('video-project.shorts'),
+        middleware: ['auth'],
+    });
+</script>
---
+++ client_browser_v2/pages/video-project/project-[id]/timeline.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ t('video-project.timeline') }}</h1>
+</template>
+
+<script setup lang="ts">
+    const { t } = useI18n();
+    definePageMeta({
+        layout: 'project',
+        title: () => t('video-project.timeline'),
+        middleware: ['auth'],
+    });
+</script>
