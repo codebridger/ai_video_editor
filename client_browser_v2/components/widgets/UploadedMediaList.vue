038a9a29bb750cda001b2a227dd207c6992e35a6
SomiVista <somayerohani06@gmail.com>
2025-03-19 13:39:23 +0400

    Add UploadedMediaList component to display uploaded media files with removal functionality

---
+++ client_browser_v2/components/widgets/UploadedMediaList.vue
@@ -0,0 +1,20 @@
+<template>
+    <section
+        :class="[
+            'dark:border-muted-700 border-y-[1px] border-solid border-y-gray-200',
+            'flex flex-wrap content-start justify-center',
+            'h-full w-full overflow-hidden overflow-y-auto',
+        ]"
+    >
+        <h1>Uploaded Media List</h1>
+        <template v-for="file in mediaManagerStore.projectFiles" :key="file._id">
+            <WidgetsProjectFileCard :file="file" @remove="mediaManagerStore.removeProjectFile" :confirm-remove="true" />
+        </template>
+    </section>
+</template>
+
+<script setup lang="ts">
+    import { useMediaManagerStore } from '../../stores/mediaManager.ts';
+
+    const mediaManagerStore = useMediaManagerStore();
+</script>
