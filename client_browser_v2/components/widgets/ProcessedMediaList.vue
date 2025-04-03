17d5f7952ed21137c30a3affd48911a37cdd13a8
SomiVista <somayerohani06@gmail.com>
2025-04-03 15:58:07 +0300

    Refactor ProcessedMediaList and timeline components to improve layout and styling, including adjustments to card classes and spacing for better visual consistency.

--- client_browser_v2/components/widgets/ProcessedMediaList.vue
+++ client_browser_v2/components/widgets/ProcessedMediaList.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="min-h-[200px] pb-2">
         <template v-for="(media, i) in mediaManagerStore.processedVideoMediaList">
-            <WidgetsProcessedMediaCard :class="['m-2 my-4 border-none bg-slate-50 dark:bg-slate-900 md:m-4', { 'mt-2': i == 0 }]" :media="media" />
+            <WidgetsProcessedMediaCard :class="['m-2 border-none bg-slate-50 dark:bg-slate-900 md:m-4', { 'mt-2': i == 0 }]" :media="media" />
         </template>
     </div>
 </template>
--- client_browser_v2/pages/video-project/project-[id]/timeline.vue
+++ client_browser_v2/pages/video-project/project-[id]/timeline.vue
@@ -2,10 +2,10 @@
     <section class="flex w-full flex-col space-y-4 p-6">
         <WidgetsPromptBox title="Prompt to generate video" v-model="prompt" :loading="isGeneratingTimeline" @generate="generateTimeline" />
         <section class="flex flex-col gap-4">
-            <Tabs :tabs="tabs" v-model="activeTab" class="my-4 min-h-max rounded border p-1">
+            <Tabs :tabs="tabs" v-model="activeTab" class="my-4 min-h-max rounded bg-white">
                 <!-- Custom content for each tab -->
                 <template #content-timeline>
-                    <div>
+                    <div class="m-2">
                         <WidgetsProjectTimeline
                             title="Clips Timeline"
                             v-model="mediaManager.timeline"
@@ -24,13 +24,7 @@
 
                 <!-- Rendered Video Revisions -->
                 <template #content-renders>
-                    <Card
-                        :class="[
-                            'rounded border-[1px] border-solid border-gray-200 p-1 dark:border-gray-700',
-                            'overflow-y-auto overflow-x-visible',
-                            'h-[550px] w-[400px]',
-                        ]"
-                    >
+                    <Card :class="['rounded', 'overflow-y-auto overflow-x-visible', 'shadow-none', 'min-h-[200px]', 'm-2']">
                         <template v-for="revision in mediaManager.videoRevisions" :key="revision._id">
                             <Card
                                 :class="[
