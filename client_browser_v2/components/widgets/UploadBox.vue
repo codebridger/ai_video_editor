fc028682aafc3ea0cd08201b2c47f3895c31c429
SomiVista <somayerohani06@gmail.com>
2025-04-05 15:55:08 +0300

    Refactor UploadBox component to improve file upload handling by changing the function signature to accept a payload object, simplifying the file processing logic, and ensuring project ID is sourced from the media manager store.

--- client_browser_v2/components/widgets/UploadBox.vue
+++ client_browser_v2/components/widgets/UploadBox.vue
@@ -7,26 +7,20 @@
 <script setup lang="ts">
     import { useMediaManagerStore } from '../../stores/mediaManager.ts';
     import { FileInputCombo } from '@codebridger/lib-vue-components/elements.ts';
-    import { useRoute } from 'vue-router';
-
     const mediaManagerStore = useMediaManagerStore();
-    const route = useRoute();
 
-    function addUploadList(files: File[]) {
-        if (!files || files.length === 0) {
+    function addUploadList(payload: { files: File[] }) {
+        if (!payload.files.length) {
             return;
         }
 
-        const projectId = route.params.id as string;
-
-        files.forEach((file) => {
+        payload.files.forEach((file) => {
             if (mediaManagerStore.uploadList.some((f) => f.name === file.name)) {
                 return;
             }
 
             mediaManagerStore.uploadList.push(file);
-            // Start upload to update projectFiles
-            mediaManagerStore.startUploadSession(file.name, projectId);
+            mediaManagerStore.startUploadSession(file.name, mediaManagerStore.projectId);
         });
     }
 </script>
